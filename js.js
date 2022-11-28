6a3433d7ff29a880c37696fe28fa67dbe336aaf4
lu7r4 <115431087+lu7r4@users.noreply.github.com>
2022-11-28 09:04:27 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+/*const pakets = document.querySelectorAll('.paket')
+for (const paket of pakets) {
+paket.addEventListener('mouseover', () => {
+    paket.classList.add('hover')
+})
+}
+for (const paket of pakets) {
+    paket.addEventListener('mouseleave', () => {
+        paket.classList.remove('hover')
+    })
+    }
+
+*/
